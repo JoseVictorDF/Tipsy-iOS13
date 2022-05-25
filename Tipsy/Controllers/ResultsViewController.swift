2bb5be99bfe1c144624f28c68faec50a54116c59
José Victor D. Fernandes <josevictordf@gmail.com>
2022-05-25 14:46:30 -0300

    Step 3

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Jose Victor Fernandes on 20/05/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+}
